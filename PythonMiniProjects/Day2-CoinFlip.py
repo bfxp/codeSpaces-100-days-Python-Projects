39c8558b1eb7db72911603edb9df676008d3e89d
Bryan <bryxjp@gmail.com>
2024-11-15 02:01:38 +0000

    Fixed the functions of both Heads/Tails (hot) and the coin function. Everything works :)

--- PythonMiniProjects/Day2-CoinFlip.py
+++ PythonMiniProjects/Day2-CoinFlip.py
@@ -1,3 +1,6 @@
+## Bryan Xaysanavongphet 11/14/2024
+
+
 import random
 
 #Creating the random coin flip function to return heads or tails.
@@ -6,38 +9,69 @@ def flip_coin():
 
 #Making a heads or tails function for the user to guess.
 def hot():
-    print("Welcome to")
+    while True:
+        #Creating an option for the user to choose either heads or tails.
+        print("Welcome to the guessing game!")
+        print("1. For Heads")
+        print("2. For Tails")
+        print("3. Quit")
+        guess = input("Please enter your guess (1 for Heads, 2 for Tails): ")
+        
+        #result of the coinflip
+        result = flip_coin()
+        
+        #if statement for when the coin is either heads or tails and letting the user know.
+        if result == "Heads" and guess == "1":
+            print("Congrats! It was heads!")
+        elif result == "Tails" and guess == "2":
+            print("Congrats! It was tails!")
+        elif guess == "3":
+            return
+        else:
+            print(f"Sorry, it was {result.lower()}.")
+
+    
+    
 
 #function for the user to clip the coin.
 def coin():
-    start = input("We're going to flip the coin now! Press 1 to start or press 2 to quit.")
-    if(start == "1"):
-        flip_coin()
-    elif (start == "2"):
-        break
-    else:
-        print("Invalid selection")
+    #Making a while loop for when the user is wanting to flip the coin.
+    while True:
+        start = input("We're going to flip the coin now! \nPress 1 to start or press 2 to quit.")
+        print("--------------------------------------")
+        
+        #if statement to show the result of the coin toss
+        if(start == "1"):
+            result = flip_coin()
+            print(f"\nThe coin landed on {result}")
+        elif (start == "2"):
+            return
+        else:
+            print("Invalid selection")
 
 
 #Main function loop.
 if __name__ == "__main__":
     
-    print("Welcome to the coin flip game!")
-    print("---------------------------------")
-    print("Please select an option below:")
-    print("1. Flip a coin.")
-    print("2. Choose if it's heads or tails.")
-    print("3. Quit.")
-    print("----------------------------------")
-    menu = input("Please select an option: ")
-    
-    if(menu == "1"):
-        coin()
-    elif (menu == "2"):
-        hot()
-    elif (menu == "3"):
-        break
-    else:
-        print("Invalid selection. Please try again.")
+    while True:
+        print("\nWelcome to the coin flip game!")
+        print("---------------------------------")
+        print("Please select an option below:")
+        print("1. Flip a coin.")
+        print("2. Choose if it's heads or tails.")
+        print("3. Quit.")
+        print("----------------------------------")
         
-    #choice = input("Please choose how many times you would like to flip the coin.")
+        menu = input("Please select an option: ")
+        
+        #menu
+        if menu == "1":
+            coin()
+        elif menu == "2":
+            hot()
+        elif menu == "3":
+            print("Thank you for playing! Goodbye!")
+            break
+        else:
+            print("Invalid selection. Please try again.")
+        
