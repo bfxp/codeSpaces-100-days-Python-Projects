9d7c10e8632ad17c1d7a931cbed1ed4b930ef5bc
Bryan <bryxjp@gmail.com>
2024-11-18 17:31:08 +0000

    Added the actual game portion to the function

--- PythonMiniProjects/Day3-RockPaperScissors.py
+++ PythonMiniProjects/Day3-RockPaperScissors.py
@@ -1,5 +1,5 @@
 ## Bryan Xaysanavongphet 11/18/24
-##Rock Paper Scissors
+## Rock Paper Scissors
 
 import random
 
@@ -14,20 +14,42 @@ def rules():
     print("Scissors beats Paper")
     print("Simple enough? I hope so!")
     
-def RPSAlgo():
-    
-   
 
 def game():
     while True:
+        print("--------------------------------------------")
         print("Time to play! Please choose an option below.")
         print("1. Rock")
         print("2. Paper")
         print("3. Scissors")
+        print("4. Quit")
         
         guess = input("Please enter your selection: ")
+        opponent = RPS()
         
-        
+        if (guess == "1" and opponent == "Rock"):
+            print("They were both rock! Please try again.")
+        elif (guess == "1" and opponent == "Paper"):
+            print("You lose! It was paper.")
+        elif (guess == "1" and opponent == "Scissors"):
+            print("You win! It was scissors.")
+        elif (guess == "2" and opponent == "Rock"):
+            print("You win! It was Rock.")
+        elif (guess == "2" and opponent == "Paper"):
+            print("They were both Paper! Please try again.")
+        elif (guess == "2" and opponent == "Scissors"):
+            print("It was Scissors! You lose!")
+        elif (guess == "3" and opponent == "Rock"):
+            print("You lose! It was rock")
+        elif (guess == "3" and opponent == "Papers"):
+            print("You win! It was paper.")
+        elif (guess == "3" and opponent == "Scissors"):
+            print("They were both Scissors! Please try again.")
+        elif (guess == "4"):
+            print("Thank you for playing!")
+            break
+        else:
+            print("Invalid selection. Please select another option.")
     
 #Main Function
 if __name__ == "__main__":
