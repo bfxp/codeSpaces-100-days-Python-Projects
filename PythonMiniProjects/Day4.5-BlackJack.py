e4270bd087c0a9968fe52d6066298f2a3267ce64
Bryan <bryxjp@gmail.com>
2024-11-19 20:44:24 +0000

    Starting on BlackJack game. Created the arrays to hold suits and ranks

--- PythonMiniProjects/Day4.5-BlackJack.py
+++ PythonMiniProjects/Day4.5-BlackJack.py
@@ -2,3 +2,12 @@
 #11/19/24
 #Going off of https://www.youtube.com/watch?v=aryte85bt_M
 
+
+
+cards = []
+suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K",]
+
+for suit in suits:
+    for rank in ranks:
+        print([suit, rank])
