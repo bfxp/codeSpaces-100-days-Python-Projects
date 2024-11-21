f924707a2efbd60b62af386659ba512928c17137
Bryan <bryxjp@gmail.com>
2024-11-21 01:37:43 +0000

    Creating a dictionary for the ranks and value

--- PythonMiniProjects/Day4.5-BlackJack.py
+++ PythonMiniProjects/Day4.5-BlackJack.py
@@ -6,7 +6,24 @@ import random
 
 cards = []
 suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K",]
+
+ranks = [
+        {"rank": "A", "value": 11},
+        {"rank": "2", "value": 2},
+        {"rank": "3", "value": 3},
+        {"rank": "4", "value": 4},
+        {"rank": "5", "value": 5},
+        {"rank": "6", "value": 6},
+        {"rank": "7", "value": 7},
+        {"rank": "8", "value": 8},
+        {"rank": "9", "value": 9},
+        {"rank": "10", "value": 10},
+        {"rank": "J", "value": 10},
+        {"rank": "Q", "value": 10},
+        {"rank": "K", "value": 10},
+    ]
+
+#for lop to add the suit and rank
 for suit in suits:
     for rank in ranks:
         cards.append([suit, rank])
@@ -27,18 +44,6 @@ def deal(number):
 
 #main
 shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
 
-#Assigning the values of the rank
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank
-
-#Key value
-rank_dict = {"rank": rank, "value": value}
-print(rank_dict["rank"], rank_dict["value"])
+card = deal(1)[0]
+print(card)
